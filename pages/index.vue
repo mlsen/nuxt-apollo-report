04cc7f0e60023a592562023eecd3ca8f81e5fa67
Marius Lueck <mluecksen@gmail.com>
2020-09-02 07:57:26 +0200

    added graphql demo

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul>
+    <li v-for="country in countries" v-bind:key="country.code">
+      {{ country.name }}
+    </li>
+  </ul>
+</template>
+
+<script>
+import gql from 'graphql-tag';
+
+export default {
+  apollo: {
+    countries: gql`query {
+      countries {
+        code
+        name
+      }
+    }`
+  }
+}
+</script>
